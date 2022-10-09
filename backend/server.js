3724f1196f8b16b535fe273b5eff974f285ca3f1
Raphico <RaphicoGit@gmail.com>
2022-10-09 16:24:45 -0700

    first commit

---
+++ backend/server.js
@@ -0,0 +1,49 @@
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+app.post('/api', (req, res) => {
+  try {
+    client.messages
+    .create({
+      body: req.body.text, 
+      from: '+15626208802', 
+      to: req.body.to
+    })
+    .then(message => {
+      res.status(200);
+      console.log(message.sid)
+    })
+  } catch (error) {
+    res.status(400);
+    console.log(error);
+  }
+})
+
+if (process.env.NODE_ENV === 'production')
+{
+  app.use(express.static(path.join(__dirname, '../', 'frontend', 'build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'));
+  })
+}
+else
+{
+  app.get('/', (req, res) => {
+    res.send('Not in production');
+  })
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started at Port ${PORT}`));
